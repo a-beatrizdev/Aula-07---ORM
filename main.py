024a7a17107b120fdcb96c75caae33a7479e97f2
Alana <alanabeatriztobias>
2026-03-13 16:19:22 -0300

    Continuação da Aula 07 sobre ORM

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Produto(Base):
     
 #Criar a conexão
 # log do banco de daods para ativar = echo=True
-engine = create_engine("sqlite:///estoque.db", echo=True) 
+engine = create_engine("sqlite:///estoque.db", echo=False) 
 
 #Criar as tabelas - se ainda não existirem
 Base.metadata.create_all(engine) 
@@ -68,19 +68,90 @@ session = Session()
 
 #CRIAR - Cadastrar
 #Como criar um objeto
-produto4 = Produto("Celular", 1500, 70, True)
+# produto1 = Produto("tablet", 2500, 20, True)
+# produto2 = Produto("celular", 2000, 550, True)
+# produto3 = Produto("mouse", 120, 10, True)
+# produto4 = Produto("teclado", 350, 33, True)
+# produto5 = Produto("pc do gabriel note", 2000, 50, True)
 
 
-session.add(produto4)
+# session.add(produto1)
+# session.add(produto2)
+# session.add(produto3)
+# session.add(produto4)
+# session.add(produto5)
 
-#Salvar no banco
-session.commit()
+
+# #Salvar no banco
+# session.commit()
 
 # Buscar produtos - listar
+# Pegar todos os registros 
 produtos = session.query(Produto).all()
 
-for produto in produtos:
-    print(produto)
+# for produto in produtos:
+#     print(produto)
+
+# Buscar apenas um único produto
+buscar_produto = session.query(Produto).filter(Produto.id == 3) .first()
+# print(buscar_produto)
+
+# Buscar os produtos com o estoque baixo
+buscar_estoque_baixo = session.query(Produto).filter(Produto.estoque == 50).all()
+# for p in buscar_estoque_baixo:
+#     print(p)
+
+# Busca parcial - considera letras maiúsculas e minúsculas
+busca_parcial = session.query(Produto).filter(Produto.nome.like("%note%")).all()
+# print(busca_parcial) 
+
+buscar_produto2 = session.query(Produto).filter_by(id=4).first()
+# print(buscar_produto2)
+
+# Top produtos com estoque baixo - limitando a busca
+top_produtos = session.query(Produto).order_by(Produto.estoque).all()
+top_produtos2 = session.query(Produto).order_by(Produto.estoque.desc()).all()
+# for p in top_produtos2:
+#     print(p)
+
+# Limitando a quantidade de registro
+top3_produtos = session.query(Produto).order_by(Produto.estoque).limit(3).all()
+# for p in top3_produtos:
+#     print(p)
+
+# Contar registros
+qtd = session.query(Produto).count()
+# print(qtd) 
+
+# UPDATE - Atualizar
+
+buscar_produto_update = session.query(Produto).filter(Produto.id == 5).first()
+
+#Atualizar um registro
+buscar_produto_update.nome = "pc gamer"
+
+session.commit()
+
+# Delete (DELETAR)
+produto_remover = session.query(Produto).filter(Produto.id == 3).first()
+
+session.delete(produto_remover)
+
+session.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
